ac9ea356aad4df35b6fc4ebd49dfb5805994def7
Mohit Bhatt <Mohit.Bhatt@buck.com>
2022-12-14 09:43:09 +0530

    update

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app=Flask(__name__)
 @app.route('/',methods=['GET','POST'])
 
 def index():
-    return 'end to end ml project'
+    return 'end to end ml project deployed using render'
 
 if __name__=="__main__":
     app.run(debug=True)
